158a7717b29457988318985bbd822c61bfcb5948
iamgauravsatija <>
2022-06-13 17:47:54 -0700

    Added new e2e test for undo feature

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -1,6 +1,11 @@
 describe('When: I use the reading list feature', () => {
+  let countBefore;
   beforeEach(() => {
     cy.startAt('/');
+    cy.get('tmo-total-count').then(($elem) => {
+      countBefore = $elem.text();
+      cy.log(countBefore, $elem.text())
+    })
   });
 
   it('Then: I should see my reading list', () => {
@@ -10,5 +15,22 @@ describe('When: I use the reading list feature', () => {
       'contain.text',
       'My Reading List'
     );
+  });
+
+
+  it('Then: I should undo addition of book to reading list', () => {
+    cy.get('input[type="search"]').type('javascript');
+    cy.get('form').submit();
+    cy.get(':nth-child(1) > .book--content > .book--content--info > :nth-child(5) > .mat-focus-indicator').click();
+    cy.get('.mat-simple-snackbar-action > .mat-focus-indicator > .mat-button-wrapper').click();
+    cy.get('tmo-total-count').should('contain.text', countBefore);
+  });
+
+  it('Then: I should undo addition of book to reading list', () => {
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get(':nth-child(1) > :nth-child(3) > .mat-focus-indicator > .mat-button-wrapper > .mat-icon').click();
+    cy.get('.mat-simple-snackbar-action > .mat-focus-indicator > .mat-button-wrapper').click();
+    cy.get('h2 > .mat-focus-indicator').click();
+    cy.get('tmo-total-count').should('contain.text', countBefore);
   });
 });
