0001954588b87ea31d85ee68e983e5440d51d490
iamgauravsatija <>
2022-06-21 12:28:57 -0700

    Moved undo test from reading-list to search-book file and improved test readability

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -17,19 +17,12 @@ describe('When: I use the reading list feature', () => {
     );
   });
 
-
-  it('Then: I should undo addition of book to reading list', () => {
+  it('Then: I should undo removal of book to reading list', () => {
     cy.get('input[type="search"]').type('javascript');
     cy.get('form').submit();
-    cy.get(':nth-child(1) > .book--content > .book--content--info > :nth-child(5) > .mat-focus-indicator').click();
-    cy.get('.mat-simple-snackbar-action > .mat-focus-indicator > .mat-button-wrapper').click();
-    cy.get('tmo-total-count').should('contain.text', countBefore);
-  });
-
-  it('Then: I should undo addition of book to reading list', () => {
     cy.get('[data-testing="toggle-reading-list"]').click();
-    cy.get(':nth-child(1) > :nth-child(3) > .mat-focus-indicator > .mat-button-wrapper > .mat-icon').click();
-    cy.get('.mat-simple-snackbar-action > .mat-focus-indicator > .mat-button-wrapper').click();
+    cy.get(':nth-child(1) > :nth-child(3) > .mat-focus-indicator').click();
+    cy.get('.mat-simple-snackbar-action > .mat-focus-indicator').click();
     cy.get('h2 > .mat-focus-indicator').click();
     cy.get('tmo-total-count').should('contain.text', countBefore);
   });
--- apps/okreads-e2e/src/integration/search-books.spec.ts
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -1,6 +1,11 @@
 describe('When: Use the search feature', () => {
+  let countBefore;
   beforeEach(() => {
     cy.startAt('/');
+    cy.get('tmo-total-count').then(($elem) => {
+      countBefore = $elem.text();
+      cy.log(countBefore, $elem.text())
+    })
   });
 
   it('Then: I should be able to search books by title', () => {
@@ -13,5 +18,14 @@ describe('When: Use the search feature', () => {
 
   xit('Then: I should see search results as I am typing', () => {
     // TODO: Implement this test!
+  });
+
+
+  it('Then: I should undo addition of book to reading list', () => {
+    cy.get('input[type="search"]').type('javascript');
+    cy.get('form').submit();
+    cy.get(':nth-child(1) > .book--content > .book--content--info > :nth-child(5) > .mat-focus-indicator').click();
+    cy.get('.mat-simple-snackbar-action > .mat-focus-indicator').click();
+    cy.get('tmo-total-count').should('contain.text', countBefore);
   });
 });
